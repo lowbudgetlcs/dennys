68c54e857a7ec789dc31a2cfc3d0369e402be2a9
ruuffian <ruuffian@gmail.com>
2024-12-17 19:39:47 -0500

    Configured Tailwindcss.

--- src/main/resources/client/src/lib/Counter.svelte
+++ src/main/resources/client/src/lib/Counter.svelte
@@ -5,6 +5,9 @@
   }
 </script>
 
-<button onclick={increment}>
+<button 
+  onclick={increment}
+  class="bg-slate-600 font-xl rounded text-white hover:bg-slate-400"
+>
   count is {count}
 </button>
