cd82aa7aa01e1f0f180c86ca8638922a08ff85b6
ruuffian <ruuffian@gmail.com>
2025-02-24 17:27:26 -0500

    Documented Series entity.

--- src/main/kotlin/com/lowbudgetlcs/entities/Series.kt
+++ src/main/kotlin/com/lowbudgetlcs/entities/Series.kt
@@ -2,6 +2,10 @@ package com.lowbudgetlcs.entities
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a series. When a series is complete, [winner]
+ * and [loser] are guarunteed non-null.
+ */
 @Serializable
 data class Series(
     override val id: SeriesId,
@@ -10,5 +14,8 @@ data class Series(
     val loser: TeamId?,
 ) : Entity<SeriesId>
 
+/**
+ * ID type for [Series].
+ */
 @Serializable
 data class SeriesId(val id: Int)
