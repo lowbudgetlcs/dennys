41cf0d29aa3fca7f5a1aa56caacf73f90dfec4d8
ruuffian <ruuffian@gmail.com>
2025-02-23 21:58:20 -0500

    Added readme template. Added documentation to criteria and AndCriteria.

--- src/main/kotlin/com/lowbudgetlcs/repositories/AndCriteria.kt
+++ src/main/kotlin/com/lowbudgetlcs/repositories/AndCriteria.kt
@@ -1,6 +1,12 @@
 package com.lowbudgetlcs.repositories
 
 class AndCriteria<T>(private val criteria1: Criteria<T>, private val criteria2: Criteria<T>) : Criteria<T> {
-
+    /**
+     * Compose two [Criteria] with an AND operation.
+     *
+     * @param[T] The type of objects to be filtered.
+     *
+     * @return A list of objects with
+     */
     override fun meetCriteria(entities: List<T>): List<T> = criteria2.meetCriteria(criteria1.meetCriteria(entities))
 }
