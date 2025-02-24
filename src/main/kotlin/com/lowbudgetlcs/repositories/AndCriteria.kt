dc665a5ef0ab1e59c715655849e6c63bc6918993
ruuffian <ruuffian@gmail.com>
2025-02-24 15:16:05 -0500

    Fixed AndCriteria docs.

--- src/main/kotlin/com/lowbudgetlcs/repositories/AndCriteria.kt
+++ src/main/kotlin/com/lowbudgetlcs/repositories/AndCriteria.kt
@@ -1,12 +1,12 @@
 package com.lowbudgetlcs.repositories
 
+/**
+ * Compose two [ICriteria] with an AND operation.
+ *
+ * @param[T] The type of objects to be filtered.
+ *
+ * @return A list of objects with
+ */
 class AndCriteria<T>(private val criteria1: ICriteria<T>, private val criteria2: ICriteria<T>) : ICriteria<T> {
-    /**
-     * Compose two [ICriteria] with an AND operation.
-     *
-     * @param[T] The type of objects to be filtered.
-     *
-     * @return A list of objects with
-     */
     override fun meetCriteria(entities: List<T>): List<T> = criteria2.meetCriteria(criteria1.meetCriteria(entities))
 }
