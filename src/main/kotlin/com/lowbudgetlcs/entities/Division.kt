658acb3a275c6b86dea5f4ea7e392dfccb21ca7b
ruuffian <ruuffian@gmail.com>
2025-02-24 17:18:44 -0500

    Documented Division entity

--- src/main/kotlin/com/lowbudgetlcs/entities/Division.kt
+++ src/main/kotlin/com/lowbudgetlcs/entities/Division.kt
@@ -3,8 +3,16 @@ package com.lowbudgetlcs.entities
 import kotlinx.serialization.Serializable
 
 
+/**
+ * Represents a Division, of the given [id] and [name].
+ * @property tournamentId The Riot-supplied tournament id associated with a [Division]. This is generated
+ * whenever a [Division] is created, and is generally immutable.
+ */
 @Serializable
-data class Division(override val id: DivisionId, val name: String, val tournamantId: Int) : Entity<DivisionId>
+data class Division(override val id: DivisionId, val name: String, val tournamentId: Int) : Entity<DivisionId>
 
+/**
+ * ID type for [Division]s.
+ */
 @Serializable
 data class DivisionId(val id: Int)
