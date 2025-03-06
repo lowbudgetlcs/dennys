d221d6556f74ee88053bc01ffe88bf5e70224866
ruuffian <ruuffian@gmail.com>
2025-03-06 18:52:35 -0500

    Moved companion object to top of class. Minor formatting changes. Added '.withExplicitSealedTypes()' to config loader to disable an annoying log message.

--- src/main/kotlin/com/lowbudgetlcs/http/RiotApiClient.kt
+++ src/main/kotlin/com/lowbudgetlcs/http/RiotApiClient.kt
@@ -10,22 +10,20 @@ import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
 class RiotApiClient(
-    private val client: HttpClient = defaultClient(),
-    private val apiKey: String = RiotConfigLoader.config.apiKey
+    private val client: HttpClient = defaultClient(), private val apiKey: String = RiotConfigLoader.config.apiKey
 ) {
+    companion object {
+        fun defaultClient(): HttpClient = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json { ignoreUnknownKeys = true })
+            }
+        }
+    }
 
     suspend fun get(url: String): HttpResponse {
         return client.get(url) {
             headers {
                 append("X-Riot-Token", apiKey)
-            }
-        }
-    }
-
-    companion object {
-        fun defaultClient(): HttpClient = HttpClient(CIO) {
-            install(ContentNegotiation) {
-                json(Json { ignoreUnknownKeys = true })
             }
         }
     }
