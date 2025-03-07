490ed201c838eef6e84622c3a0a4fcd6cf4fbfd6
ruuffian <ruuffian@gmail.com>
2025-03-06 20:01:46 -0500

    Fixed imports. Changed some logging levels to prevent spam in production.

--- src/main/kotlin/com/lowbudgetlcs/workers/TournamentEngine.kt
+++ src/main/kotlin/com/lowbudgetlcs/workers/TournamentEngine.kt
@@ -2,14 +2,13 @@ package com.lowbudgetlcs.workers
 
 import com.lowbudgetlcs.bridges.LblcsDatabaseBridge
 import com.lowbudgetlcs.bridges.RabbitMQBridge
+import com.lowbudgetlcs.http.RiotApiClient
 import com.lowbudgetlcs.models.Game
 import com.lowbudgetlcs.models.TeamId
-import com.lowbudgetlcs.http.RiotApiClient
 import com.lowbudgetlcs.repositories.AndCriteria
 import com.lowbudgetlcs.repositories.games.*
 import com.lowbudgetlcs.repositories.players.AllPlayersDatabase
 import com.lowbudgetlcs.repositories.players.IPlayerRepository
-import com.lowbudgetlcs.repositories.riot.RiotMatchRepository
 import com.lowbudgetlcs.repositories.riot.MatchRepositoryRiot
 import com.lowbudgetlcs.repositories.series.AllSeriesDatabase
 import com.lowbudgetlcs.repositories.series.ISeriesRepository
@@ -34,7 +33,7 @@ class TournamentEngine private constructor(
     private val gamesR: IGameRepository,
     private val seriesR: ISeriesRepository,
     private val playersR: IPlayerRepository,
-    private val riotMatchRepository: RiotMatchRepository
+    private val riotMatchRepository: MatchRepositoryRiot
 ) : AbstractWorker(), IMessageQListener {
 
     private val logger: Logger = LoggerFactory.getLogger(TournamentEngine::class.java)
@@ -76,10 +75,10 @@ class TournamentEngine private constructor(
      */
     override fun processMessage(delivery: Delivery) {
         val message = String(delivery.body, charset("UTF-8"))
-        logger.info("📩 Received message: $message")
+        logger.info("📩 TournamentEngine received message!")
         try {
             val callback = Json.decodeFromString<RiotCallback>(message)
-            logger.info("✅ Successfully decoded RiotCallback for game ID: ${callback.gameId}")
+            logger.debug("✅ Successfully decoded RiotCallback for game ID: ${callback.gameId}")
             CoroutineScope(Dispatchers.IO).launch {
                 processRiotCallback(callback)
             }
@@ -100,7 +99,7 @@ class TournamentEngine private constructor(
      * save the winner and loser of the series.
      */
     private suspend fun processRiotCallback(callback: RiotCallback) {
-        logger.info("🔍 Fetching match details for game ID: ${callback.gameId}")
+        logger.info("🔍 Fetching match details for game ID: ${callback.gameId}...")
 
         val tournamentMatch = riotMatchRepository.getMatch(callback.gameId)
         tournamentMatch?.let { match ->
@@ -112,7 +111,7 @@ class TournamentEngine private constructor(
 
                     if (winner == null || loser == null) throw IllegalArgumentException("TeamId not found.")
 
-                    logger.info("🏆 Winner: $winner, ❌ Loser: $loser")
+                    logger.debug("\uD83C\uDFC6 Winner: {}, ❌ Loser: {}", winner, loser)
 
                     updateGame(callback, winner, loser)?.let {
                         updateSeries(
@@ -130,7 +129,7 @@ class TournamentEngine private constructor(
      * Updates the game in storage derived from [callback] with a [winner] and [loser].
      */
     private fun updateGame(callback: RiotCallback, winner: TeamId, loser: TeamId): Game? {
-        logger.info("📝 Updating game record for shortcode: ${callback.shortCode}")
+        logger.info("📝 Updating game record for shortcode: ${callback.shortCode}...")
 
         gamesR.readByCriteria(ShortcodeCriteria(callback.shortCode)).first().let { game ->
             val updatedGame = game.copy(winner = winner, loser = loser, callbackResult = callback)
@@ -142,7 +141,9 @@ class TournamentEngine private constructor(
      * Updates the series in storage that owns [game] with a winner and loser.
      */
     private fun updateSeries(game: Game, team1: TeamId, team2: TeamId) {
+        logger.info("📝 Updating series record for derived from: ${game.id}...")
         seriesR.readById(game.series)?.let { series ->
+            logger.debug("\uD83D\uDCDD Updating series record: {}...", series.id)
             // Magic number yayyyy! This needs an actual solution- for now the app only supports Bo3.
             val winCondition = 2
             val team1Wins = gamesR.readByCriteria(
@@ -152,7 +153,7 @@ class TournamentEngine private constructor(
                 AndCriteria(TeamWinCriteria(team2), SeriesCriteria(game.series))
             ).size
 
-            fun logWin(team: TeamId) = logger.debug("🏅 Team $team wins series ${series.id}!")
+            fun logWin(team: TeamId) = logger.debug("\uD83C\uDFC5 Team {} wins series {}!", team, series.id)
             when (winCondition) {
                 team1Wins -> {
                     seriesR.update(series.copy(winner = team1, loser = team2))
@@ -165,7 +166,7 @@ class TournamentEngine private constructor(
                 }
                 // Otherwise, series has not concluded
                 else -> {
-                    logger.info("⏳ Series ${series.id} is still ongoing...")
+                    logger.debug("⏳ Series {} is still ongoing...", series.id)
                 }
             }
         }
