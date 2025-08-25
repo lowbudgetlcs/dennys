6d27b766d8da125556c3957913ba03f159236784
ruuffian <ruuffian@gmail.com>
2025-08-25 16:06:56 -0400

    Unit tests pass, testing manually now.

--- src/test/kotlin/services/SeriesServiceTest.kt
+++ src/test/kotlin/services/SeriesServiceTest.kt
@@ -3,8 +3,15 @@ package services
 import com.lowbudgetlcs.domain.models.NewSeries
 import com.lowbudgetlcs.domain.models.Series
 import com.lowbudgetlcs.domain.models.SeriesId
+import com.lowbudgetlcs.domain.models.events.Event
 import com.lowbudgetlcs.domain.models.events.EventId
+import com.lowbudgetlcs.domain.models.events.EventStatus
+import com.lowbudgetlcs.domain.models.events.toEventId
+import com.lowbudgetlcs.domain.models.riot.tournament.toRiotTournamentId
+import com.lowbudgetlcs.domain.models.team.Team
 import com.lowbudgetlcs.domain.models.team.TeamId
+import com.lowbudgetlcs.domain.models.team.toTeamId
+import com.lowbudgetlcs.domain.models.team.toTeamName
 import com.lowbudgetlcs.domain.services.SeriesService
 import com.lowbudgetlcs.repositories.ISeriesRepository
 import com.lowbudgetlcs.repositories.ITeamRepository
@@ -16,6 +23,8 @@ import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import java.time.Instant
+import kotlin.test.expect
 
 class SeriesServiceTest : StringSpec({
     val teamRepo = mockk<ITeamRepository>(relaxed = false)
@@ -24,21 +33,42 @@ class SeriesServiceTest : StringSpec({
 
     beforeTest { clearAllMocks() }
 
+    val event = Event(
+        id = 0.toEventId(),
+        name = "Test",
+        description = "",
+        riotTournamentId = 0.toRiotTournamentId(),
+        createdAt = Instant.now(),
+        startDate = Instant.now(),
+        endDate = Instant.now().plusSeconds(6L),
+        status = EventStatus.ACTIVE,
+        eventGroupId = null
+    )
+    val participatingTeams = listOf(
+        Team(
+            id = 1.toTeamId(), name = "Test".toTeamName(), eventId = event.id, logoName = null
+        ), Team(
+            id = 2.toTeamId(), name = "Test 2".toTeamName(), eventId = event.id, logoName = null
+        )
+    )
+    val expectedSeries = Series(
+        id = SeriesId(1),
+        gamesToWin = 3,
+        eventId = event.id,
+        participants = participatingTeams.map { it.id },
+        result = null
+    )
+
+    val newSeries = NewSeries(
+        eventId = event.id, participantIds = participatingTeams.map { it.id }, gamesToWin = 3
+    )
     "createSeries succeeds for valid input" {
-        val participatingTeams = listOf(TeamId(1), TeamId(2))
-        val eventId = EventId(3)
-        val created = Series(
-            id = SeriesId(1), gamesToWin = 3, eventId = eventId, participants = participatingTeams,
-            result = null
-        )
 
-        val newSeries = NewSeries(
-            eventId = eventId, participantIds = participatingTeams, gamesToWin = 3
-        )
+        every { seriesRepo.insert(newSeries) } returns expectedSeries
+        every { teamRepo.getById(participatingTeams[0].id) } returns participatingTeams[0]
+        every { teamRepo.getById(participatingTeams[1].id) } returns participatingTeams[1]
 
-        every { seriesRepo.insert(newSeries) } returns created
-
-        service.createSeries(newSeries) shouldBe created
+        service.createSeries(newSeries) shouldBe expectedSeries
 
         verify(exactly = 1) { seriesRepo.insert(newSeries) }
     }
@@ -46,9 +76,7 @@ class SeriesServiceTest : StringSpec({
     "createSeries fails for 0 games to win" {
         shouldThrow<IllegalArgumentException> {
             service.createSeries(
-                NewSeries(
-                    eventId = EventId(1), participantIds = listOf(TeamId(1), TeamId(2)), gamesToWin = 0
-                )
+                newSeries.copy(gamesToWin = 0)
             )
         }
         // repo.insert should never be called
@@ -57,15 +85,17 @@ class SeriesServiceTest : StringSpec({
 
     "getAllTeams returns repo data" {
         val series = listOf(
-            Series(SeriesId(1), EventId(1), 3, null, null), Series(SeriesId(2), EventId(1), 3, TeamId(1), TeamId(2))
+            Series(SeriesId(1), EventId(1), 3, participatingTeams.map { it.id }, null), Series(
+                SeriesId(2), EventId(1), 3, participatingTeams.map { it.id }, null
+            )
         )
 
-        every { seriesRepo.getAllFromEvent(EventId(1)) } returns series
+        every { seriesRepo.getAllByEventId(EventId(1)) } returns series
 
         val result = service.getAllSeriesFromEvent(EventId(1))
         result.map { it.id } shouldContainExactly listOf(SeriesId(1), SeriesId(2))
 
-        verify(exactly = 1) { seriesRepo.getAllFromEvent(EventId(1)) }
+        verify(exactly = 1) { seriesRepo.getAllByEventId(EventId(1)) }
     }
 
     "getSeries throws for unknown id" {
@@ -73,7 +103,7 @@ class SeriesServiceTest : StringSpec({
         every { seriesRepo.getById(id) } returns null
 
         val ex = shouldThrow<NoSuchElementException> { service.getSeries(id) }
-        ex.message shouldBe "Team not found"
+        ex.message shouldBe "Series not found"
 
         verify(exactly = 1) { seriesRepo.getById(id) }
     }
