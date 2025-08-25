e4e2a53c4634f00a8afa8e569b83ce80514f4b59
ruuffian <ruuffian@gmail.com>
2025-08-25 16:39:28 -0400

    All seeding scripts completed.

---
+++ scripts/data/series.json.ex
@@ -0,0 +1,17 @@
+{
+  "division 1": {
+    "team 1": [
+      "team 2"
+    ],
+    "team 2": [
+      "team 1"
+    ]
+  "division 2": {
+    "team 3": [
+      "team 4"
+    ],
+    "team 4": [
+      "team 3"
+    ]
+  }
+}
--- scripts/divisionsSeed.py
+++ scripts/divisionsSeed.py
@@ -5,6 +5,7 @@ import url
 import json
 import requests
 from argparse import ArgumentParser
+from filters  import getEventId, getTeamId
 
 parser = ArgumentParser()
 parser = url.args(parser)
@@ -15,39 +16,9 @@ res = requests.get(f"{url}/event", headers=header)
 res.raise_for_status()
 EVENTS = res.json()
 
-
-def getEventId(name):
-    e = [x["id"] for x in EVENTS if x["name"] == name]
-    count = len(e)
-    if count > 1:
-        print(f"More than one event matched {name}")
-        print(e)
-        return None
-    elif count == 0:
-        print(f"No event matched {name}")
-        return None
-    else:
-        return e[0]
-
-
 res = requests.get(f"{url}/team", headers=header)
 res.raise_for_status()
 TEAMS = res.json()
-
-
-def getTeamId(name):
-    t = [x["id"] for x in TEAMS if x["name"] == name]
-    count = len(t)
-    if count > 1:
-        print(f"More than one team matched {name}")
-        print(t)
-        return None
-    elif count == 0:
-        print(f"No team matched {name}")
-        return None
-    else:
-        return t[0]
-
 
 prefix = "Season 15"
 with open("data/divisions.json") as f:
@@ -55,12 +26,12 @@ with open("data/divisions.json") as f:
     for div, teams in data.items():
         # Create each team
         division = f"{prefix} {div}"
-        eventId = getEventId(division)
+        eventId = getEventId(division, EVENTS)
         if eventId is None:
             continue
         for teamName in teams:
             try:
-                teamId = getTeamId(teamName)
+                teamId = getTeamId(teamName, TEAMS)
                 if teamId is None:
                     continue
                 payload = {"teamId": teamId}
