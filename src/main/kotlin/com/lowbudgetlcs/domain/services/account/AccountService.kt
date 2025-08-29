c408afd45df95aa36b60038678ce53523eb519f1
ruuffian <ruuffian@gmail.com>
2025-08-29 14:53:24 -0400

    Added logs to account service.

--- src/main/kotlin/com/lowbudgetlcs/domain/services/account/AccountService.kt
+++ src/main/kotlin/com/lowbudgetlcs/domain/services/account/AccountService.kt
@@ -7,23 +7,30 @@ import com.lowbudgetlcs.domain.models.riot.RiotPuuid
 import com.lowbudgetlcs.gateways.riot.account.IRiotAccountGateway
 import com.lowbudgetlcs.repositories.DatabaseException
 import com.lowbudgetlcs.repositories.account.IAccountRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class AccountService(
     private val accountRepository: IAccountRepository, private val riotAccountGateway: IRiotAccountGateway
 ) : IAccountService {
+    private val logger: Logger by lazy { LoggerFactory.getLogger(this::class.java) }
 
     override fun getAccount(accountId: RiotAccountId): RiotAccount {
+        logger.debug("Fetching account '$accountId'...")
         return accountRepository.getById(accountId) ?: throw NoSuchElementException("Account not found")
     }
 
-    override fun getAllAccounts(): List<RiotAccount> = accountRepository.getAll()
+    override fun getAllAccounts(): List<RiotAccount> {
+        logger.debug("Fetching all accounts...")
+        return accountRepository.getAll()
+    }
 
     override suspend fun createAccount(account: NewRiotAccount): RiotAccount {
+        logger.debug("Creating new account...")
+        logger.debug(account.toString())
         val puuid = account.riotPuuid
 
-        if (isPuuidTaken(puuid)) {
-            throw IllegalStateException("Riot account already exists")
-        }
+        if (isPuuidTaken(puuid)) throw IllegalStateException("Riot account already exists")
 
         // Call Riot API to verify PUUID
         riotAccountGateway.getAccountByPuuid(puuid.value) // throws if anything fails
@@ -32,6 +39,7 @@ class AccountService(
     }
 
     fun isPuuidTaken(puuid: RiotPuuid): Boolean {
+        logger.debug("Checking if '$puuid' is taken...")
         return accountRepository.getAccountByPuuid(puuid.value) != null
     }
 
