0ec5ad3d36e2eab002177ac5e4b6a4628dfbdcbe
ruuffian <ruuffian@gmail.com>
2025-08-29 15:22:31 -0400

    Added logging to gateways.

--- src/main/kotlin/com/lowbudgetlcs/gateways/riot/account/RiotAccountGateway.kt
+++ src/main/kotlin/com/lowbudgetlcs/gateways/riot/account/RiotAccountGateway.kt
@@ -1,12 +1,14 @@
 package com.lowbudgetlcs.gateways.riot.account
 
-import com.lowbudgetlcs.domain.models.riot.RiotApiException
 import com.lowbudgetlcs.api.dto.riot.account.RiotAccountDto
+import com.lowbudgetlcs.domain.models.riot.RiotApiException
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class RiotAccountGateway(
     private val client: HttpClient,
@@ -14,7 +16,9 @@ class RiotAccountGateway(
     private val baseUrl: String = "https://americas.api.riotgames.com"
 ) : IRiotAccountGateway {
 
+    private val logger: Logger by lazy { LoggerFactory.getLogger(this::class.java) }
     override suspend fun getAccountByPuuid(puuid: String): RiotAccountDto {
+        logger.debug("Fetching account for '$puuid'...")
         val response: HttpResponse = client.get("$baseUrl/riot/account/v1/accounts/by-puuid/$puuid") {
             headers {
                 append("X-Riot-Token", apiKey)
@@ -22,10 +26,17 @@ class RiotAccountGateway(
         }
 
         return when (response.status) {
-            HttpStatusCode.OK -> response.body<RiotAccountDto>()
+            HttpStatusCode.OK -> {
+                logger.debug("Successfully fetched account.")
+                response.body<RiotAccountDto>()
+            }
+
             HttpStatusCode.BadRequest -> throw IllegalArgumentException("Invalid Riot PUUID")
             HttpStatusCode.NotFound -> throw NoSuchElementException("Riot account not found for PUUID")
-            else -> throw RiotApiException("Unexpected Riot API error: ${response.status}")
+            else -> {
+                logger.warn("Failed to fetch account.")
+                throw RiotApiException("Unexpected Riot API error: ${response.status}")
+            }
         }
     }
 }
--- src/main/kotlin/com/lowbudgetlcs/gateways/riot/tournament/RiotTournamentGateway.kt
+++ src/main/kotlin/com/lowbudgetlcs/gateways/riot/tournament/RiotTournamentGateway.kt
@@ -1,5 +1,8 @@
 package com.lowbudgetlcs.gateways.riot.tournament
 
+import com.lowbudgetlcs.api.dto.riot.tournament.RiotShortcodeDto
+import com.lowbudgetlcs.api.dto.riot.tournament.RiotTournamentParametersDto
+import com.lowbudgetlcs.api.dto.riot.tournament.toShortcodeParametersDto
 import com.lowbudgetlcs.domain.models.riot.RiotApiException
 import com.lowbudgetlcs.domain.models.riot.tournament.NewShortcode
 import com.lowbudgetlcs.domain.models.riot.tournament.RiotTournament
@@ -7,14 +10,13 @@ import com.lowbudgetlcs.domain.models.riot.tournament.RiotTournamentId
 import com.lowbudgetlcs.domain.models.riot.tournament.toRiotTournamentId
 import com.lowbudgetlcs.repositories.DatabaseException
 import com.lowbudgetlcs.repositories.metadata.IMetadataRepository
-import com.lowbudgetlcs.api.dto.riot.tournament.RiotShortcodeDto
-import com.lowbudgetlcs.api.dto.riot.tournament.RiotTournamentParametersDto
-import com.lowbudgetlcs.api.dto.riot.tournament.toShortcodeParametersDto
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 
 class RiotTournamentGateway(
@@ -27,8 +29,10 @@ class RiotTournamentGateway(
     private val url: String by lazy {
         if (useStubs) "$baseUrl/lol/tournament-stub/v5" else "$baseUrl/lol/tournament/v5"
     }
+    private val logger: Logger by lazy { LoggerFactory.getLogger(this::class.java) }
 
     override suspend fun create(tournamentName: String): RiotTournament {
+        logger.debug("Creating tournament named '$tournamentName'...")
         val providerId = metadataRepo.getProviderId() ?: throw DatabaseException("Cannot find riot provider id.")
         val res = client.post("$url/tournaments") {
             headers {
@@ -51,6 +55,9 @@ class RiotTournamentGateway(
     override suspend fun getCode(
         riotTournamentId: RiotTournamentId, newShortcode: NewShortcode
     ): RiotShortcodeDto {
+        logger.debug("Fetching tournament code for tournament '$riotTournamentId'...")
+        val body = newShortcode.toShortcodeParametersDto()
+        logger.debug(body.toString())
         val res: HttpResponse = client.post("$url/codes") {
             url {
                 parameters.append("tournamentId", "${riotTournamentId.value}")
@@ -60,15 +67,17 @@ class RiotTournamentGateway(
                 append("X-Riot-Token", apiKey)
             }
             contentType(ContentType.Application.Json)
-            setBody(newShortcode.toShortcodeParametersDto())
+            setBody(body)
         }
         when (res.status) {
             HttpStatusCode.OK -> {
+                logger.debug("Successfully created codes.")
                 val codes = res.body<List<String>>()
                 return RiotShortcodeDto(codes)
             }
 
             else -> {
+                logger.warn("Failed to create codes.")
                 throw RiotApiException("Unexpected Riot API error: ${res.status}")
             }
         }
