41c074f6db840b8ba467a91edd768d78a021aff6
ruuffian <ruuffian@gmail.com>
2026-05-04 01:44:53 -0400

    Migrated EventGroupRoutes to DI. Fixed DI binding bug where routes were overwriting each other and not registering correctly.

--- src/main/kotlin/com/lowbudgetlcs/api/routes/Api.kt
+++ src/main/kotlin/com/lowbudgetlcs/api/routes/Api.kt
@@ -1,7 +1,7 @@
 package com.lowbudgetlcs.api.routes
 
 import com.lowbudgetlcs.domain.ApiRoute
-import com.lowbudgetlcs.domain.event.adapter.`in`.web.eventGroupRoutesV1
+import com.lowbudgetlcs.logger
 import io.ktor.server.auth.authenticate
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.route
@@ -9,12 +9,13 @@ import org.koin.ktor.ext.getKoin
 
 fun Routing.apiRoutes() {
     val routes: List<ApiRoute> = getKoin().getAll<ApiRoute>()
+    logger.debug("Available routes: {}", routes)
     route("/api/v1") {
         authenticate("auth-session", "auth-token") {
             routes.forEach { route ->
+                logger.debug("Registering {}...", route)
                 route.register(this)
             }
-            eventGroupRoutesV1()
         }
     }
 }
--- src/main/kotlin/com/lowbudgetlcs/config/modules/ApiRoutes.kt
+++ src/main/kotlin/com/lowbudgetlcs/config/modules/ApiRoutes.kt
@@ -2,17 +2,21 @@ package com.lowbudgetlcs.config.modules
 
 import com.lowbudgetlcs.domain.ApiRoute
 import com.lowbudgetlcs.domain.account.adapter.`in`.web.AccountRoutesV1
+import com.lowbudgetlcs.domain.event.adapter.`in`.web.EventGroupRoutesV1
 import com.lowbudgetlcs.domain.event.adapter.`in`.web.EventRoutesV1
 import com.lowbudgetlcs.domain.player.adapter.`in`.web.PlayerRoutesV1
 import com.lowbudgetlcs.domain.series.adapter.`in`.web.SeriesRoutesV1
 import com.lowbudgetlcs.domain.team.adapter.`in`.web.TeamRoutesV1
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 
 val apiRouteModule = module {
-    single<ApiRoute> { EventRoutesV1(get(), get()) }
-    single<ApiRoute> { TeamRoutesV1(get()) }
-    single<ApiRoute> { PlayerRoutesV1(get()) }
-    single<ApiRoute> { AccountRoutesV1(get()) }
-    single<ApiRoute> { SeriesRoutesV1(get()) }
+    singleOf(::EventRoutesV1) { bind<ApiRoute>() }
+    singleOf(::TeamRoutesV1) { bind<ApiRoute>() }
+    singleOf(::PlayerRoutesV1) { bind<ApiRoute>() }
+    singleOf(::AccountRoutesV1) { bind<ApiRoute>() }
+    singleOf(::SeriesRoutesV1) { bind<ApiRoute>() }
+    singleOf(::EventGroupRoutesV1) { bind<ApiRoute>() }
 }
