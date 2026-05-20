c6ec5871e803613c90ecedfa6a2fca17313fcf65
AlirpJ <prila.jsp@gmail.com>
2026-05-20 10:26:53 -0500

    Add SQL Account create scripts

---
+++ src/migrations/sql/Account_Combat.sql
@@ -0,0 +1,31 @@
+--info:particpants [0]
+
+CREATE TABLE Account_Combat (
+
+spell1Casts INTEGER,
+spell2Casts INTEGER,
+spell3Casts INTEGER,
+spell4Casts INTEGER,
+
+kills INTEGER,
+deaths INTEGER,
+doubleKills INTEGER,
+tripleKills INTEGER,
+quadraKills INTEGER,
+pentaKills INTEGER,
+
+magicDamageDealt NUMERIC(10,2),
+magicDamageTaken NUMERIC(10,2),
+physicalDamageDealt NUMERIC(10,2),
+physicalDamageTaken NUMERIC(10,2),
+
+firstBloodAssist BOOLEAN,
+firstBloodKill BOOLEAN,
+goldEarned NUMERIC(10,2),
+
+objectivesStolen INTEGER,
+dragonKills INTEGER,
+
+damageDealtToTurrets NUMERIC(10,2),
+damageSelfMitigated NUMERIC(10,2),
+);
---
+++ src/migrations/sql/Account_Pings.sql
@@ -0,0 +1,22 @@
+--info:particpants [0]
+
+CREATE TABLE Account_Pings (
+
+riotIdGameName TEXT(90) NOT NULL,
+riotIdTagline TEXT(60),
+
+allInPings INTEGER,
+assistMePings INTEGER,
+basicPings INTEGER,
+
+commandPings INTEGER,
+dangerPings INTEGER,
+enemyMissingPings INTEGER,
+enemyVisionPings INTEGER,
+holdPings INTEGER,
+getBackPings INTEGER,
+needVisionPings INTEGER,
+onMyWayPings INTEGER,
+pushPings INTEGER,
+retreatPings INTEGER
+);
